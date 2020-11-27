3617b0cc934caf895f955a6463ecc2fda9b75037
Ubaid-Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-11-28 01:23:49 +0530

    Greetings

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,10 @@
 // write code for Welcome component here
+import React from "react";
+const name = (props) => (
+  <>
+    <h1>Hey ! {props.name} </h1>
+    <h2> Welcome to Newton School.</h2>
+  </>
+);
+
+export default name;
