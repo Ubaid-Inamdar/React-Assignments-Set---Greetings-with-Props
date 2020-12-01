6d2c88d1fc15e97e3ca3c53559fc6c6d9e647480
Ubaid Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-12-01 17:00:44 +0530

    full stop removed

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React from "react";
 const name = (props) => (
   <>
     <h1>Hey !{props.name}</h1>
-    <h2>Welcome to Newton School.</h2>
+    <h2>Welcome to Newton School</h2>
   </>
 );
 
